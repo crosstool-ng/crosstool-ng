65bb3ac67c741c46aa27ccd98cb7930a2b731306
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2008-12-16 18:12:34 +0000

    Get rid of the tols/ directory: - move config.guess and config.sub from tools/ into scripts/ - update the scripts and makefile fragments accordingly
     /trunk/Makefile.in        |   10     5     5     0 +++++-----
     /trunk/scripts/functions  |   16     8     8     0 ++++++++--------
     /trunk/scripts/scripts.mk |   28    13    15     0 +++++++++++++---------------
     /trunk/ct-ng.in           |    4     2     2     0 ++--
     4 files changed, 28 insertions(+), 30 deletions(-)

--- scripts/scripts.mk
+++ scripts/scripts.mk
@@ -1,4 +1,4 @@
-# Makefile for the tools/ sub-directory
+# Makefile for the scripts/ sub-directory
 
 # Here, we can update the config.* scripts.
 # If we're in CT_LIB_DIR, then CT_LIB_DIR == CT_TOP_DIR, and we can update those
@@ -16,17 +16,9 @@ help-distrib::
 # The tools are: config.guess and config.sub
 
 CONFIG_SUB_SRC="http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD"
-CONFIG_SUB_DEST=tools/config.sub
+CONFIG_SUB_DEST=scripts/config.sub
 CONFIG_GUESS_SRC="http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD"
-CONFIG_GUESS_DEST=tools/config.guess
-
-# Kludge: we have a step that is called 'tools', and thus we can not define a
-# rule here that is named 'tools'. Naming it 'tools/' does not help either.
-# Thus, prepend the top directory to have a single rule description. This is
-# not as bad as it seems, because it is only refered to by teo rules in this
-# file, but still it is a kludge...
-$(CT_TOP_DIR)/tools:
-	$(SILENT)mkdir -p tools
+CONFIG_GUESS_DEST=scripts/config.guess
 
 PHONY += updatetools
 updatetools: $(CONFIG_SUB_DEST) $(CONFIG_GUESS_DEST)
@@ -39,12 +31,18 @@ ifeq ($(strip $(V)),2)
   wget_opt=
 endif
 
-$(CONFIG_SUB_DEST): .FORCE $(CT_TOP_DIR)/tools
+PHONY += scripts
+scripts:
+	$(SILENT)mkdir -p scripts
+
+FROCE += $(CONFIG_SUB_DEST)
+$(CONFIG_SUB_DEST): scripts
 	@$(ECHO) '  WGET  $@'
 	$(SILENT)wget $(wget_opt) -O $@ $(CONFIG_SUB_SRC)
 	$(SILENT)chmod u+rwx,go+rx-w $@
 
-$(CONFIG_GUESS_DEST): .FORCE $(CT_TOP_DIR)/tools
+FORCE += $(CONFIG_GUESS_DEST)
+$(CONFIG_GUESS_DEST): scripts
 	@$(ECHO) '  WGET  $@'
 	$(SILENT)wget $(wget_opt) -O $@ $(CONFIG_GUESS_SRC)
 	$(SILENT)chmod u+rwx,go+rx-w $@
@@ -53,5 +51,5 @@ $(CONFIG_GUESS_DEST): .FORCE $(CT_TOP_DIR)/tools
 # Clean up the mess
 
 distclean::
-	@$(ECHO) "  CLEAN tools"
-	$(SILENT)[ $(CT_TOP_DIR) = $(CT_LIB_DIR) ] || rm -rf $(CT_TOP_DIR)/tools
+	@$(ECHO) "  CLEAN scripts"
+	$(SILENT)[ $(CT_TOP_DIR) = $(CT_LIB_DIR) ] || rm -rf $(CT_TOP_DIR)/scripts
