8d993f036a4859dc021284c04f2578d8e48c799d
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2009-03-08 11:15:58 +0000

    Use the common functions (CT_GetSVN, CT_Extract and CT_Patch) to retrieve, extract and patch sstrip from buildroot.
     /trunk/scripts/build/tools/200-sstrip.sh |   16     3    13     0 +++-------------
     1 file changed, 3 insertions(+), 13 deletions(-)

--- scripts/build/tools/200-sstrip.sh
+++ scripts/build/tools/200-sstrip.sh
@@ -28,21 +28,11 @@ case "${CT_SSTRIP_FROM}" in
 
     buildroot)
         do_tools_sstrip_get() {
-            # We have to retrieve sstrip.c from a viewVC web interface. This
-            # is not handled by the common CT_GetFile, thus we must take all
-            # steps taken by CT_GetFile ourselves:
-
-            CT_GetLocal sstrip .c && return 0 || true
-            CT_TestAndAbort "File '${file}' not present locally, and downloads are not allowed" "${CT_FORBID_DOWNLOAD}" = "y"
-            CT_DoLog EXTRA "Retrieving 'sstrip'"
-            CT_DoGetFile "http://sources.busybox.net/index.py/trunk/buildroot/toolchain/sstrip/sstrip.c?view=co"
-            mv "sstrip.c?view=co" "${CT_TARBALLS_DIR}/sstrip.c"
-            CT_SaveLocal "${CT_TARBALLS_DIR}/sstrip.c"
+            CT_GetSVN sstrip svn://uclibc.org/trunk/buildroot/toolchain/sstrip
         }
         do_tools_sstrip_extract() {
-            # We'll let buildroot guys take care of sstrip maintenance and patching.
-            mkdir -p "${CT_SRC_DIR}/sstrip"
-            CT_DoExecLog ALL cp -v "${CT_TARBALLS_DIR}/sstrip.c" "${CT_SRC_DIR}/sstrip"
+            CT_Extract sstrip
+            CT_Patch sstrip
         }
         do_tools_sstrip_build() {
             CT_DoStep INFO "Installing sstrip"
