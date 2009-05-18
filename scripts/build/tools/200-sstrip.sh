170db5c2ed90ea8619e8639a2e11aec3519f3319
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2009-05-18 16:51:20 +0000

    1.4: backport #1519 from /trunk: - sstrip: update the upstream URL, now buildroot has migrated to   using git instead of svn.
     -------- diffstat follows --------
     /branches/1.4/scripts/build/tools/200-sstrip.sh |   11     8     3     0 ++++++++---
     1 file changed, 8 insertions(+), 3 deletions(-)

--- scripts/build/tools/200-sstrip.sh
+++ scripts/build/tools/200-sstrip.sh
@@ -28,11 +28,16 @@ case "${CT_SSTRIP_FROM}" in
 
     buildroot)
         do_tools_sstrip_get() {
-            CT_GetSVN sstrip svn://uclibc.org/trunk/buildroot/toolchain/sstrip
+            CT_GetFile sstrip .c http://git.buildroot.net/buildroot/plain/toolchain/sstrip
         }
         do_tools_sstrip_extract() {
-            CT_Extract sstrip
-            CT_Patch sstrip
+            # We leave the sstrip maintenance to the buildroot people:
+            # -> any fix-up goes directly there
+            # -> we don't have patches for it
+            # -> we don't need to patch it
+            # -> just create a directory in src/, and copy it there.
+            CT_DoExecLog DEBUG mkdir -p "${CT_SRC_DIR}/sstrip"
+            CT_DoExecLog DEBUG cp -v "${CT_TARBALLS_DIR}/sstrip.c" "${CT_SRC_DIR}/sstrip"
         }
         do_tools_sstrip_build() {
             CT_DoStep INFO "Installing sstrip"
