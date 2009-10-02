399a88364fc3ae6555d90a8ccb47feb6d2efaf53
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2009-10-02 22:10:17 +0200

    libc/eglibc: fix building for seemingly native toolchains
    Build eglibc with -O2 as a fix/workaround to building
    seemingly-native toolchains (see docs/overview.txt).
    
    See:
    - docs/overview.txt
    - docs/known-issues.txt
    - http://sourceware.org/ml/crossgcc/2009-10/msg00001.html

--- scripts/build/libc/eglibc.sh
+++ scripts/build/libc/eglibc.sh
@@ -252,7 +252,7 @@ do_libc() {
     CT_DoLog DEBUG "Extra CC args passed    : '${extra_cc_args}'"
 
     BUILD_CC="${CT_BUILD}-gcc"                                      \
-    CFLAGS="${CT_TARGET_CFLAGS} ${CT_LIBC_GLIBC_EXTRA_CFLAGS} -O"   \
+    CFLAGS="${CT_TARGET_CFLAGS} ${CT_LIBC_GLIBC_EXTRA_CFLAGS} -O2"  \
     CC="${CT_TARGET}-gcc ${CT_LIBC_EXTRA_CC_ARGS} ${extra_cc_args}" \
     AR=${CT_TARGET}-ar                                              \
     RANLIB=${CT_TARGET}-ranlib                                      \
