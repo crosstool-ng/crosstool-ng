2cde67ddc4f34c499428911bdb9479b0c00c546a
harold <harold@dev.site>
2009-11-14 15:03:01 -0600

    arch: add s390 and s390x architectures and samples

---
+++ scripts/build/arch/s390.sh
@@ -0,0 +1,9 @@
+# Compute s390-specific values
+
+CT_DoArchTupleValues() {
+    # The architecture part of the tuple:
+    # Explicit settings not required for the default value "s390"
+    #CT_TARGET_ARCH="s390"
+    #CT_KERNEL_ARCH="s390"
+    return 0
+}
---
+++ scripts/build/arch/s390x.sh
@@ -0,0 +1,8 @@
+# Compute s390x-specific values
+
+CT_DoArchTupleValues() {
+    # The architecture part of the tuple:
+    # Explicit settings not required for the default value "s390x"
+    #CT_TARGET_ARCH="s390x"
+    CT_KERNEL_ARCH="s390"
+}
