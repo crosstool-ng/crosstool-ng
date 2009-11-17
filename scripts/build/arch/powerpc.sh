28b38f922241bd273f4f71823c037d4af43d96ba
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2009-11-17 11:33:59 +0100

    arch/ppc: commonalise the 32- and 64-bit PowerPC arch

--- scripts/build/arch/powerpc.sh
+++ scripts/build/arch/powerpc.sh
@@ -1,3 +1,21 @@
 # Compute powerpc-specific values
 
-. "${CT_LIB_DIR}/scripts/build/arch/powerpc-powerpc64.sh-common"
+CT_DoArchTupleValues () {
+    # The architecture part of the tuple, override only for 64-bit
+    if [ "${CT_ARCH_64}" = "y" ]; then
+        CT_TARGET_ARCH="powerpc64"
+    fi
+
+    # Add spe in the tuple if needed
+    case "${CT_LIBC},${CT_ARCH_POWERPC_SPE}" in
+        glibc,|eglibc,)   CT_TARGET_SYS=gnu;;
+        glibc,y|eglibc,y) CT_TARGET_SYS=gnuspe;;
+    esac
+
+    # Add extra flags for SPE if needed
+    if [ "${CT_ARCH_POWERPC_SPE}" = "y" ]; then
+        CT_ARCH_TARGET_CFLAGS="-mabi=spe -mspe"
+        CT_ARCH_CC_CORE_EXTRA_CONFIG="--enable-e500_double"
+        CT_ARCH_CC_EXTRA_CONFIG="--enable-e500_double"
+    fi
+}
--- scripts/build/arch/powerpc64.sh
+++
@@ -1,3 +0,0 @@
-# Compute powerpc-specific values
-
-. "${CT_LIB_DIR}/scripts/build/arch/powerpc-powerpc64.sh-common"
