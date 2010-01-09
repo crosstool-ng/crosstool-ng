fbd2a88e6e5579f7c014cc48b866c782534bf004
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2010-01-09 15:57:07 +0100

    arch/alpha: cleanup Alpha code

--- scripts/build/arch/alpha.sh
+++ scripts/build/arch/alpha.sh
@@ -3,7 +3,4 @@
 CT_DoArchTupleValues () {
     # The architecture part of the tuple:
     CT_TARGET_ARCH="${CT_ARCH}${CT_ARCH_ALPHA_VARIANT}"
-
-    # The kernel ARCH:
-    CT_KERNEL_ARCH=${CT_ARCH}
 }
