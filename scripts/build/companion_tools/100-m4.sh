e3a8a010257e87ae18fa2ce55746faa3871f232a
Richard Strand <richard.strand@icomera.com>
2010-01-12 21:47:14 +0000

    companion_tools/m4: Add m4 tool
    Add version 1.4.13 of m4 as a companion tool
    
    Signed-off-by: Richard Strand <richard.strand@icomera.com>

---
+++ scripts/build/companion_tools/100-m4.sh
@@ -0,0 +1,27 @@
+# Build script for m4
+
+CT_M4_VERSION=1.4.13
+
+do_companion_tools_m4_get() {
+    CT_GetFile "m4-${CT_M4_VERSION}" \
+               {ftp,http}://ftp.gnu.org/gnu/m4
+}
+
+do_companion_tools_m4_extract() {
+    CT_Extract "m4-${CT_M4_VERSION}"
+    CT_Patch "m4-${CT_M4_VERSION}"
+}
+
+do_companion_tools_m4_build() {
+    CT_DoStep EXTRA "Installing m4"
+    mkdir -p "${CT_BUILD_DIR}/build-m4"
+    CT_Pushd "${CT_BUILD_DIR}/build-m4"
+    
+    CT_DoExecLog ALL \
+    "${CT_SRC_DIR}/m4-${CT_M4_VERSION}/configure" \
+        --prefix="${CT_TOOLS_OVERIDE_DIR}"
+    CT_DoExecLog ALL make
+    CT_DoExecLog ALL make install
+    CT_Popd
+    CT_EndStep
+}
