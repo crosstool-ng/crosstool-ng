933404b4492264018b40228dfa79ea2ccee30bbd
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2010-02-04 15:18:46 +0100

    cc/gcc: simplify the _or_later stuff

--- scripts/addToolVersion.sh
+++ scripts/addToolVersion.sh
@@ -100,12 +100,11 @@ addToolVersion() {
             ver_M=$(echo "${version}...." |cut -d . -f 1)
             ver_m=$(echo "${version}...." |cut -d . -f 2)
             if [    ${ver_M} -gt 4                          \
-                 -o \( ${ver_M} -eq 4 -a ${ver_m} -ge 3 \)  ]; then
-                SedExpr1="${SedExpr1}\n    select CC_GCC_4_3_or_later"
-            fi
-            if [    ${ver_M} -gt 4                          \
                  -o \( ${ver_M} -eq 4 -a ${ver_m} -ge 4 \)  ]; then
                 SedExpr1="${SedExpr1}\n    select CC_GCC_4_4_or_later"
+            elif [    ${ver_M} -gt 4                          \
+                   -o \( ${ver_M} -eq 4 -a ${ver_m} -ge 3 \)  ]; then
+                SedExpr1="${SedExpr1}\n    select CC_GCC_4_3_or_later"
             fi
             ;;
         uClibc)
