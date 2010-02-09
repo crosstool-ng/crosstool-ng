c6689215d15cba83404b6f4e67ddedb2c2ea1a48
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2010-02-09 22:50:53 +0100

    binutils/binutils: fix using GMP and MPFR

--- scripts/build/binutils/binutils.sh
+++ scripts/build/binutils/binutils.sh
@@ -85,9 +85,8 @@ do_binutils_target() {
         install_targets+=("install-${t}")
     done
 
-
     # If GMP and MPFR were configured, then use that
-    if [ "${CT_GMP_MPFR_TARGET}" = "y" ]; then
+    if [ "${CT_BINUTILS_TARGET_USE_GMP_MPFR}" = "y" ]; then
         extra_config+=("--with-gmp=${CT_SYSROOT_DIR}/usr")
         extra_config+=("--with-mpfr=${CT_SYSROOT_DIR}/usr")
     fi
