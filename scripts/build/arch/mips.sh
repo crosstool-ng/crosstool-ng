f8ba6cb7e68aaac83c1a05dd934904ff20d3dd71
Zhuang Yuyao <mlistz@gmail.com>
2010-02-20 07:35:02 +0100

    arch: add mips64
    This adds initial mips64 config option and build script.

--- scripts/build/arch/mips.sh
+++ scripts/build/arch/mips.sh
@@ -1,8 +1,13 @@
 # Compute IA-64-specific values
 
 CT_DoArchTupleValues() {
-    # The architecture part of the tuple:
-    CT_TARGET_ARCH="${CT_ARCH}${target_endian_el}"
+    # The architecture part of the tuple, override only for 64-bit
+    if [ "${CT_ARCH_64}" = "y" ]; then
+        CT_TARGET_ARCH="mips64${target_endian_el}"
+    else
+        # The architecture part of the tuple:
+        CT_TARGET_ARCH="${CT_ARCH}${target_endian_el}"
+    fi
 
     # Override CFLAGS for endianness:
     case "${CT_ARCH_BE},${CT_ARCH_LE}" in
