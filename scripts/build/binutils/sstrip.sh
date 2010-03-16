5b8dcf755be217e35767db5861beba9b45f60a84
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2010-03-17 00:28:14 +0100

    binutils/sstrip: get rid of ELFkickers
    ELFkickers are looong dead and unmaintained, and the sstrip
    from buildroot is working fine *and* is maintained.

--- scripts/build/binutils/sstrip.sh
+++ scripts/build/binutils/sstrip.sh
@@ -4,57 +4,32 @@ do_tools_sstrip_get() { :; }
 do_tools_sstrip_extract() { :; }
 do_tools_sstrip() { :; }
 
-case "${CT_SSTRIP_FROM}" in
-    ELFkickers)
-        do_sstrip_get() {
-            CT_GetFile "ELFkickers-${CT_SSTRIP_ELFKICKERS_VERSION}"     \
-                       http://www.muppetlabs.com/~breadbox/pub/software
-        }
-        do_sstrip_extract() {
-            CT_Extract "ELFkickers-${CT_SSTRIP_ELFKICKERS_VERSION}"
-            CT_Patch "ELFkickers-${CT_SSTRIP_ELFKICKERS_VERSION}"
-        }
-        do_sstrip() {
-            CT_DoStep INFO "Installing sstrip"
-            CT_DoExecLog ALL cp -av "${CT_SRC_DIR}/ELFkickers-${CT_SSTRIP_ELFKICKERS_VERSION}/sstrip"   \
-                                    "${CT_BUILD_DIR}/build-strip"
-            cd "${CT_BUILD_DIR}/build-strip"
+if [ "${CT_SSTRIP}" = "y" ]; then
+    do_sstrip_get() {
+        CT_GetFile sstrip .c http://git.buildroot.net/buildroot/plain/toolchain/sstrip
+    }
 
-            CT_DoLog EXTRA "Building sstrip"
-            CT_DoExecLog ALL make CC="${CT_HOST}-gcc" sstrip
-            
-            CT_DoLog EXTRA "Installing sstrip"
-            CT_DoExecLog ALL install -m 755 sstrip "${CT_PREFIX_DIR}/bin/${CT_TARGET}-sstrip"
+    do_sstrip_extract() {
+        # We leave the sstrip maintenance to the buildroot people:
+        # -> any fix-up goes directly there
+        # -> we don't have patches for it
+        # -> we don't need to patch it
+        # -> just create a directory in src/, and copy it there.
+        CT_DoExecLog DEBUG mkdir -p "${CT_SRC_DIR}/sstrip"
+        CT_DoExecLog DEBUG cp -v "${CT_TARBALLS_DIR}/sstrip.c" "${CT_SRC_DIR}/sstrip"
+    }
 
-            CT_EndStep
-        }
-    ;;
+    do_sstrip() {
+        CT_DoStep INFO "Installing sstrip"
+        mkdir -p "${CT_BUILD_DIR}/build-sstrip"
+        cd "${CT_BUILD_DIR}/build-sstrip"
 
-    buildroot)
-        do_sstrip_get() {
-            CT_GetFile sstrip .c http://git.buildroot.net/buildroot/plain/toolchain/sstrip
-        }
-        do_sstrip_extract() {
-            # We leave the sstrip maintenance to the buildroot people:
-            # -> any fix-up goes directly there
-            # -> we don't have patches for it
-            # -> we don't need to patch it
-            # -> just create a directory in src/, and copy it there.
-            CT_DoExecLog DEBUG mkdir -p "${CT_SRC_DIR}/sstrip"
-            CT_DoExecLog DEBUG cp -v "${CT_TARBALLS_DIR}/sstrip.c" "${CT_SRC_DIR}/sstrip"
-        }
-        do_sstrip() {
-            CT_DoStep INFO "Installing sstrip"
-            mkdir -p "${CT_BUILD_DIR}/build-sstrip"
-            cd "${CT_BUILD_DIR}/build-sstrip"
+        CT_DoLog EXTRA "Building sstrip"
+        CT_DoExecLog ALL "${CT_HOST}-gcc" -Wall -o sstrip "${CT_SRC_DIR}/sstrip/sstrip.c"
 
-            CT_DoLog EXTRA "Building sstrip"
-            CT_DoExecLog ALL "${CT_HOST}-gcc" -Wall -o sstrip "${CT_SRC_DIR}/sstrip/sstrip.c"
+        CT_DoLog EXTRA "Installing sstrip"
+        CT_DoExecLog ALL install -m 755 sstrip "${CT_PREFIX_DIR}/bin/${CT_TARGET}-sstrip"
 
-            CT_DoLog EXTRA "Installing sstrip"
-            CT_DoExecLog ALL install -m 755 sstrip "${CT_PREFIX_DIR}/bin/${CT_TARGET}-sstrip"
-
-            CT_EndStep
-        }
-    ;;
-esac
+        CT_EndStep
+    }
+fi
