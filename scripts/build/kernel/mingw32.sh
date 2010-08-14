1c81b216c8eb5071747a3cd1c3f94ce96aac19a5
Esben Haabendal <eha@doredevelopment.dk>
2010-08-14 23:14:27 +0200

    kernel/mingw32: use pre-set headers dir
    There is a variable that contains the headers directory,
    so there's no need to re-compute it again.
    
    [Yann E. MORIN: split the original patch]

--- scripts/build/kernel/mingw32.sh
+++ scripts/build/kernel/mingw32.sh
@@ -19,9 +19,9 @@ do_kernel_extract() {
 do_kernel_headers() {
     CT_DoStep INFO "Installing kernel headers"
 
-    mkdir -p "${CT_SYSROOT_DIR}/include"
-    cp -r ${CT_SRC_DIR}/w32api-${CT_W32API_VERSION}-mingw32/include \
-          ${CT_SYSROOT_DIR}
+    mkdir -p "${CT_HEADERS_DIR}"
+    cp -r ${CT_SRC_DIR}/w32api-${CT_W32API_VERSION}-mingw32/include/* \
+          ${CT_HEADERS_DIR}
 
     CT_EndStep
 }
