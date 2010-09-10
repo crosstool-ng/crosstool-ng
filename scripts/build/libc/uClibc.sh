3fcdd28e048a0b5333175509d119c7450b918ca9
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2010-09-10 13:23:58 +0200

    libc/uClibc: change munging code based on target arch
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>

--- scripts/build/libc/uClibc.sh
+++ scripts/build/libc/uClibc.sh
@@ -282,20 +282,22 @@ mungeuClibcConfig() {
 		ENDSED
 
     # Ah. We may one day need architecture-specific handler here...
-    if [ "${CT_ARCH}" = "arm" ]; then
-        # Hack the ARM {E,O}ABI into the config file
-        if [ "${CT_ARCH_ARM_EABI}" = "y" ]; then
-            cat <<-ENDSED
-				s/.*(CONFIG_ARM_OABI).*/# \\1 is not set/
-				s/.*(CONFIG_ARM_EABI).*/\\1=y/
-				ENDSED
-        else
-            cat <<-ENDSED
-				s/.*(CONFIG_ARM_OABI).*/\\1=y/
-				s/.*(CONFIG_ARM_EABI).*/# \\1 is not set/
-				ENDSED
-        fi
-    fi
+    case "${CT_ARCH}" in
+        arm)
+            # Hack the ARM {E,O}ABI into the config file
+            if [ "${CT_ARCH_ARM_EABI}" = "y" ]; then
+                cat <<-ENDSED
+					s/.*(CONFIG_ARM_OABI).*/# \\1 is not set/
+					s/.*(CONFIG_ARM_EABI).*/\\1=y/
+					ENDSED
+            else
+                cat <<-ENDSED
+					s/.*(CONFIG_ARM_OABI).*/\\1=y/
+					s/.*(CONFIG_ARM_EABI).*/# \\1 is not set/
+					ENDSED
+            fi
+            ;;
+    esac
 
     # Accomodate for old and new uClibc versions, where the
     # way to select between big/little endian has changed
