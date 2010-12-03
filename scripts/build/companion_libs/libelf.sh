1b3c15e7ec296f9d146a6eb09d8fce469df754e7
Kalle Kankare <kalle.kankare@vincit.fi>
2010-12-03 12:36:44 +0100

    complibs/libelf: use host compiler
    It appears, that the configure scripts of libelf versions 0.8.13 and
    0.8.12 do not honour the --host option. The compiler must be given as an
    environment variable or the process will use the command "gcc" as the
    compiler.
    
    It seems that this is already done in the function do_libelf_target in
    scripts/build/companion_libs/libelf.sh, but not in function do_libelf.
    (transplanted from 81b4c0efa46f757b89c150f66e09615ee7c0f5a4)

--- scripts/build/companion_libs/libelf.sh
+++ scripts/build/companion_libs/libelf.sh
@@ -36,6 +36,7 @@ do_libelf() {
         libelf_opts+=( --disable-shared --enable-static )
     fi
 
+    CC="${CT_HOST}-gcc"                                     \
     CT_DoExecLog CFG                                        \
     "${CT_SRC_DIR}/libelf-${CT_LIBELF_VERSION}/configure"   \
         --build=${CT_BUILD}                                 \
