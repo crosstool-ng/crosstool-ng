6fea2cfd092e6825d0df397425d8236317e46475
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2011-09-17 14:30:52 +0200

    gcc: add support for the PowerPC EABI
    Signed-off-by: Mircea Gherzan <mgherzan@gmail.com>
    [yann.morin.1998@anciens.enib.fr: rm trailing space]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>

--- scripts/build/arch/powerpc.sh
+++ scripts/build/arch/powerpc.sh
@@ -6,14 +6,20 @@ CT_DoArchTupleValues () {
         CT_TARGET_ARCH="powerpc64"
     fi
 
-    # Add spe in the tuple if needed
-    case "${CT_LIBC},${CT_ARCH_POWERPC_SPE}" in
-        glibc,|eglibc,)   CT_TARGET_SYS=gnu;;
-        glibc,y|eglibc,y) CT_TARGET_SYS=gnuspe;;
+    CT_TARGET_SYS="gnu"
+    case "${CT_ARCH_powerpc_ABI}" in
+        "") ;;
+        eabi) CT_TARGET_SYS="eabi";;
+        spe)
+            case "${CT_LIBC}" in
+                glibc|eglibc) CT_TARGET_SYS="gnuspe";;
+                *)            CT_TARGET_SYS="spe";
+            esac
+            ;;
     esac
 
     # Add extra flags for SPE if needed
-    if [ "${CT_ARCH_POWERPC_SPE}" = "y" ]; then
+    if [ "${CT_ARCH_powerpc_ABI_SPE}" = "y" ]; then
         CT_ARCH_TARGET_CFLAGS="-mabi=spe -mspe"
         CT_ARCH_CC_CORE_EXTRA_CONFIG="--enable-e500_double"
         CT_ARCH_CC_EXTRA_CONFIG="--enable-e500_double"
