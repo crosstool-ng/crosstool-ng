f8a9bc67602f0908862cb33514f4943853d77fc2
Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
2011-10-16 14:58:29 +0200

    complibs/cloog: fix extraction
    In the early days, cloog-ppl was bizarrely packaged: the first tarball
    did not contain the version in the name of the extracted directory, so
    we had to play tricks.
    
    Nowadays, however, the first component of the path are stripped when
    extracting a  tarball, which means that the created directory will
    always be properly named. So, our old tricks do no longer work, and
    worse, they break the build.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
    (transplanted from f070d922facf42b0395b7db909e1867d45b245dd)

--- scripts/build/companion_libs/cloog.sh
+++ scripts/build/companion_libs/cloog.sh
@@ -19,31 +19,22 @@ do_cloog_get() {
 do_cloog_extract() {
     local _t
 
-    CT_Extract "cloog-ppl-${CT_CLOOG_VERSION}"
-
     # Version 0.15.3 has a dirname 'cloog-ppl' (with no version in it!)
     # while versions 0.15.4 onward do have the version in the dirname.
-    case "${CT_CLOOG_VERSION}" in
-        0.15.3) _t="";;
-        *)      _t="-${CT_CLOOG_VERSION}";;
-    esac
-    CT_Pushd "${CT_SRC_DIR}/cloog-ppl${_t}"
-    CT_Patch nochdir "cloog-ppl" "${CT_CLOOG_VERSION}"
+    # But, because the infrastructure properly creates the extracted
+    # directories (with tar's --strip-components), we can live safely...
+    CT_Extract "cloog-ppl-${CT_CLOOG_VERSION}"
+    CT_Patch "cloog-ppl" "${CT_CLOOG_VERSION}"
+
     if [ "${CT_CLOOG_VERSION}" = "0.15.10" ]; then
+        CT_Pushd "${CT_SRC_DIR}/cloog-ppl-${CT_CLOOG_VERSION}"
         CT_DoExecLog CFG ./autogen.sh
+        CT_Popd
     fi
-    CT_Popd
 }
 
 do_cloog() {
-    local _t
-
-    # Version 0.15.3 has a dirname 'cloog-ppl' (with no version in it!)
-    # while versions 0.15.4 onward do have the version in the dirname.
-    case "${CT_CLOOG_VERSION}" in
-        0.15.3) _t="";;
-        *)      _t="-${CT_CLOOG_VERSION}";;
-    esac
+    local cloog_src_dir="${CT_SRC_DIR}/cloog-ppl-${CT_CLOOG_VERSION}"
 
     mkdir -p "${CT_BUILD_DIR}/build-cloog-ppl"
     cd "${CT_BUILD_DIR}/build-cloog-ppl"
@@ -55,7 +46,7 @@ do_cloog() {
     CT_DoExecLog CFG                            \
     CFLAGS="${CT_CFLAGS_FOR_HOST}"              \
     LDFLAGS="-lm"                               \
-    "${CT_SRC_DIR}/cloog-ppl${_t}/configure"    \
+    "${cloog_src_dir}/configure"                \
         --build=${CT_BUILD}                     \
         --host=${CT_HOST}                       \
         --prefix="${CT_COMPLIBS_DIR}"           \
