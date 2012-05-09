4e8f04012c9161b92d4dab3e32930dec6140a7df
Yann E. MORIN" <yann.morin.1998@free.fr>
2012-05-09 18:17:17 +0200

    cc/gcc: do not build manuals in parallel
    Reported-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
    Reported-by: Johannes Stezenbach <js@sig21.net>
    Tested-by: Johannes Stezenbach <js@sig21.net>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>

--- scripts/build/cc/gcc.sh
+++ scripts/build/cc/gcc.sh
@@ -801,7 +801,7 @@ do_cc_backend() {
 
     if [ "${build_manuals}" = "yes" ]; then
         CT_DoLog EXTRA "Building the GCC manuals"
-        CT_DoExecLog ALL make ${JOBSFLAGS} pdf html
+        CT_DoExecLog ALL make pdf html
         CT_DoLog EXTRA "Installing the GCC manuals"
         CT_DoExecLog ALL make install-{pdf,html}-gcc
     fi
