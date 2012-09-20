395dca5ea86a34df73ccdaee61b6516df06070f4
David Holsgrove <david.holsgrove@xilinx.com>
2012-09-20 14:01:38 +1000

    arch/microblaze: add new architecture
    Add Microblaze architecture support.
    
    This depends on EXPERIMENTAL, as upstream projects do not yet
    include full support to build a modern microblaze compiler.
    This is in the process of being updated, but is not currently
    publicly accessible.
    
    Signed-off-by: "David Holsgrove" <david.holsgrove@xilinx.com>
    Message-Id: <9c93e18b3d68b19303f3.1348113870@localhost.localdomain>
    PatchWork-ID: 185305

---
+++ scripts/build/arch/microblaze.sh
@@ -0,0 +1,21 @@
+# Compute microblaze specific values
+
+CT_DoArchTupleValues () {
+    # The architecture part of the tuple:
+    CT_TARGET_ARCH="${CT_ARCH}${target_endian_el}"
+
+    # gcc ./configure flags
+    CT_ARCH_WITH_ARCH=
+    CT_ARCH_WITH_ABI=
+    CT_ARCH_WITH_CPU=
+    CT_ARCH_WITH_TUNE=
+    CT_ARCH_WITH_FPU=
+    CT_ARCH_WITH_FLOAT=
+
+    # CFLAGS
+    case "${CT_ARCH_FLOAT_HW},${CT_ARCH_FLOAT_SW}" in
+        y,) CT_ARCH_FLOAT_CFLAG="-mhard-float" ;;
+        ,y) CT_ARCH_FLOAT_CFLAG="-msoft-float" ;;
+    esac
+
+}
