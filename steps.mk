0be070e5b7f435ed13494560016061315a992516
Yann Diorcet (diorcet yann <Yann Diorcet (diorcet.yann@gmail.com)>
2012-11-16 14:59:27 +0100

    complibs: introduce generic multi-complibs infrastructure
    Use the same method as companion tools for providing generic and
    extendable companion libs.
    
    Signed-off-by: Yann Diorcet <diorcet.yann@gmail.com>
    Message-Id: <515c5c4635d99ebe4877.1353074410@macbook-smorlat.local>
    Patchwork-Id: 199613

--- steps.mk
+++ steps.mk
@@ -18,37 +18,27 @@ help-env::
 # but are actual steps for canadian and cross-native.
 # Please keep the last line with a '\' and keep the following empy line:
 # it helps when diffing and merging.
-CT_STEPS := libc_check_config   \
-            gmp_for_build       \
-            mpfr_for_build      \
-            ppl_for_build       \
-            cloog_for_build     \
-            mpc_for_build       \
-            libelf_for_build    \
-            binutils_for_build  \
-            elf2flt_for_build   \
-            gmp_for_host        \
-            mpfr_for_host       \
-            ppl_for_host        \
-            cloog_for_host      \
-            mpc_for_host        \
-            libelf_for_host     \
-            binutils_for_host   \
-            elf2flt_for_host    \
-            sstrip_for_host     \
-            cc_core_pass_1      \
-            kernel_headers      \
-            libc_start_files    \
-            cc_core_pass_2      \
-            libc                \
-            cc_for_build        \
-            cc_for_host         \
-            libc_finish         \
-            libelf_for_target   \
-            binutils_for_target \
-            debug               \
-            test_suite          \
-            finish              \
+CT_STEPS := libc_check_config        \
+            companion_libs_for_build \
+            binutils_for_build       \
+            elf2flt_for_build        \
+            companion_libs_for_host  \
+            binutils_for_host        \
+            elf2flt_for_host         \
+            sstrip_for_host          \
+            cc_core_pass_1           \
+            kernel_headers           \
+            libc_start_files         \
+            cc_core_pass_2           \
+            libc                     \
+            cc_for_build             \
+            cc_for_host              \
+            libc_finish              \
+            libelf_for_target        \
+            binutils_for_target      \
+            debug                    \
+            test_suite               \
+            finish                   \
 
 # Keep an empty line above this comment, so the last
 # back-slash terminated line works as expected.
