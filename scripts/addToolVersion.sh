340875e473ccc1cbfce13529ad2cbb5ffc0660f7
Yann E. MORIN" <yann.morin.1998@free.fr>
2012-12-26 12:15:10 +0100

    libc/eglibc: remove now superfluous config knobs
    All eglibc versions we now have support pkgversion and bugurl.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>

--- scripts/addToolVersion.sh
+++ scripts/addToolVersion.sh
@@ -143,16 +143,8 @@ addToolVersion() {
             # Extract 'M'ajor and 'm'inor from version string
             ver_M=$(getVersionField "${version}" . 1)
             ver_m=$(getVersionField "${version}" . 2)
-            if [   \( ${ver_M} -eq 2 -a ${ver_m} -eq 13 \)  ]; then
-                SedExpr1="${SedExpr1}\n    select LIBC_EGLIBC_2_13_or_later"
-            elif [ \( ${ver_M} -eq 2 -a ${ver_m} -eq 12 \)  ]; then
-                SedExpr1="${SedExpr1}\n    select LIBC_EGLIBC_2_12_or_later"
-            elif [ \( ${ver_M} -eq 2 -a ${ver_m} -eq 11 \)  ]; then
-                SedExpr1="${SedExpr1}\n    select LIBC_EGLIBC_2_11_or_later"
-            elif [ \( ${ver_M} -eq 2 -a ${ver_m} -eq 10 \)  ]; then
-                SedExpr1="${SedExpr1}\n    select LIBC_EGLIBC_2_10_or_later"
-            elif [ \( ${ver_M} -eq 2 -a ${ver_m} -eq 9 \)  ]; then
-                SedExpr1="${SedExpr1}\n    select LIBC_EGLIBC_2_9_or_later"
+            if [   \( ${ver_M} -eq 2 -a ${ver_m} -ge 16 \)  ]; then
+                SedExpr1="${SedExpr1}\n    select LIBC_EGLIBC_2_16_or_later"
             fi
             ;;
         uClibc)
