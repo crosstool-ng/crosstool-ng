16a5cc49e0ff0e59ecca170a07317cf265722ee2
Ray Donnelly <mingw.android@gmail.com>
2014-04-12 13:16:01 +0100

    arch/all: Add common function to return multilib target
    This code was abstracted out of Cody P Schafer's multilib patch.
    It doesn't seem right having architecture dependent code in a
    specific libc implementation script. So this patch breaks it out into
    scripts/build/arch/<arch>.sh in a function:
    
      multilib_target_to_build="$(CT_DoArchMultilibTarget 'multi_flags' 'target-in')"
    
    Changed by Alexey Neyman:
    - make option analysis check specific option rather than match global
      options string as a whole. Moreover, old code did not handle multiple
      options in the same multilib, e.g. '-m64 -mlittle'.
    - fixed substitutions in powerpc.sh (*le variants did not match the
      pattern in the shell parameter expansion)
    - make s390.sh actually apply the flags it gathered from the options.
    - straighten the spaghetti in x86.sh by setting two flags, arch & abi.
      Also, do not depend on "gnu" being the last part - we can have
      '*-uclibcx32', for example.
    
    Signed-off-by: Bryan Hundven <bryanhundven@gmail.com>
    Signed-off-by: Ray Donnelly <ray.donnelly@gmail.com>
    Signed-off-by: Alexey Neyman <stilor@att.net>

--- scripts/build/arch/x86.sh
+++ scripts/build/arch/x86.sh
@@ -20,4 +20,49 @@ CT_DoArchTupleValues() {
         esac
     fi
     CT_TARGET_ARCH="${CT_TARGET_ARCH}${CT_ARCH_SUFFIX}"
+
+    # Shouldn't be possible to specify this (CT_TARGET_SYS is not specified by the user,
+    # it is computed by scripts/functions from libc choices). But trap if such invalid
+    # values ever come from the caller:
+    case "${CT_TARGET_ARCH}-${CT_TARGET_SYS}" in
+        i[34567]86-gnux32)
+            CT_DoLog ERROR "Invalid CT_TARGET: i[34567]86-<vendor>-<os>-gnux32 is invalid."
+            CT_DoLog ERROR "CT_TARGET: ${CT_TARGET}"
+            CT_Abort "Go read: https://wiki.debian.org/Multiarch/Tuples"
+            ;;
+    esac
+}
+
+#------------------------------------------------------------------------------
+# Get multilib architecture-specific target
+# Usage: CT_DoArchMultilibTarget "multilib flags" "target tuple"
+CT_DoArchMultilibTarget ()
+{
+    local multi_flags="${1}"
+    local target="${2}"
+
+    local bit=default
+    local abi=default
+
+    for m in $multi_flags; do
+        case "$m" in
+            -m32)  bit=32; abi=default;;
+            -m64)  bit=64; abi=default;;
+            -mx32) bit=64; abi=x32;;
+        esac
+    done
+
+    # Fix up architecture.
+    case "${target}" in
+        x86_64-*)      [ $bit = 32 ] && target=${target/#x86_64-/i386-} ;;
+        i[34567]86-*)  [ $bit = 64 ] && target=${target/#i[34567]86-/x86_64-} ;;
+    esac
+
+    # Fix up the ABI part.
+    case "${target}" in
+        *x32) [ $abi = default ] && target=${target/%x32} ;;
+        *)    [ $abi = x32 ] && target=${target}x32 ;;
+    esac
+
+    echo "${target}"
 }
