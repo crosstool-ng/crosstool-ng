9d44f05699d2b643db8b1b2c1efd24e89f71771a
Max Filippov <jcmvbkbc@gmail.com>
2019-03-04 07:26:01 -0800

    overlay: treat overlay as a package
    This allows downloading overlay from an URL or getting it from a custom
    location. Getting overlay source from VCS is not very useful and thus
    is not implemented.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ scripts/build/overlay.sh
@@ -0,0 +1,7 @@
+# This file adds functions to get xtensa overlay
+# Licensed under the GPL v2. See COPYING in the root of this package
+
+# Download overlay
+do_overlay_get() {
+    CT_Fetch OVERLAY
+}
