8e588f639d98b229b72fcf73eb0b1e316d1f1b16
takumin <takumiiinn@gmail.com>
2019-09-05 21:40:05 +0900

    initial implemented musl-fts multilib build

--- scripts/build/companion_libs/340-musl_fts.sh
+++ scripts/build/companion_libs/340-musl_fts.sh
@@ -18,14 +18,8 @@ do_musl_fts_extract() {
 
 # Build musl-fts for running on target
 do_musl_fts_for_target() {
-    local -a musl_fts_opts
-
     CT_DoStep INFO "Installing musl-fts for target"
-    CT_mkdir_pushd "${CT_BUILD_DIR}/build-musl-fts-target-${CT_TARGET}"
-
-    do_musl_fts_backend "${musl_fts_opts[@]}"
-
-    CT_Popd
+    CT_IterateMultilibs do_musl_fts_backend multilib
     CT_EndStep
 }
 
@@ -33,38 +27,58 @@ do_musl_fts_for_target() {
 #     Parameter     : description               : type      : default
 do_musl_fts_backend() {
     local arg
+    local multi_dir        # GCC internal library location for the multilib
+    local multi_os_dir     # OS library location for the multilib
+    local multi_os_dir_gcc # Same as multi_os_dir, preserved from GCC output
+    local multi_flags      # CFLAGS for this multilib
+    local multi_root       # Sysroot for this multilib
+    local multi_target     # Target tuple, either as reported by GCC or by our guesswork
+    local multi_index      # Index of the current multilib
+    local multi_count      # Total number of multilibs
+    local hdr_install_subdir
     local -a extra_config
+    local -a extra_cflags
 
     for arg in "$@"; do
         eval "${arg// /\\ }"
     done
 
-    CT_DoLog EXTRA "Configuring musl-fts"
+    CT_mkdir_pushd "${CT_BUILD_DIR}/build-musl-fts-target-${multi_index}-${multi_count}"
 
-    if [ "${CT_SHARED_LIBS}" = "y" ]; then
-        extra_config+=( --enable-shared )
-    else
-        extra_config+=( --disable-shared )
+    CT_DoStep INFO "Building for multilib ${multi_index}/${multi_count}: '${multi_flags}'"
+
+    multilib_dir="/usr/lib/${multi_os_dir}"
+    CT_SanitizeVarDir multilib_dir
+    CT_DoExecLog ALL mkdir -p "${multi_root}${multilib_dir}"
+
+    extra_cflags=( ${multi_flags} )
+
+    CT_DoArchMUSLHeaderDir hdr_install_subdir "${multi_flags}"
+    if [ -n "${hdr_install_subdir}" ]; then
+      extra_config+=( "--includedir=/usr/include/${hdr_install_subdir}" )
     fi
 
-    CT_DoExecLog CFG                      \
-    CC="${CT_TARGET}-gcc"                 \
-    CFLAGS="${CT_ALL_TARGET_CFLAGS}"      \
-    LDFLAGS="${CT_ALL_TARGET_LDFLAGS}"    \
-    ${CONFIG_SHELL}                       \
-    "${CT_SRC_DIR}/musl-fts/configure"    \
-        --build=${CT_BUILD}               \
-        --host="${CT_TARGET}"             \
-        --target=${CT_TARGET}             \
-        --prefix=${CT_SYSROOT_DIR_PREFIX} \
-        --enable-static                   \
-        "${extra_config[@]}"
+    CT_SymlinkToolsMultilib
+
+    CT_DoLog EXTRA "Configuring musl-fts"
+    CT_DoExecLog CFG                     \
+        CC="${CT_TARGET}-gcc"            \
+        CFLAGS="${extra_cflags[*]}"      \
+        ${CONFIG_SHELL}                  \
+        ${CT_SRC_DIR}/musl-fts/configure \
+            --host="${multi_target}"     \
+            --target="${multi_target}"   \
+            --prefix="/usr"              \
+            --libdir="${multilib_dir}"   \
+            "${extra_config[@]}"
 
     CT_DoLog EXTRA "Building musl-fts"
     CT_DoExecLog ALL make ${CT_JOBSFLAGS}
 
     CT_DoLog EXTRA "Installing musl-fts"
-    CT_DoExecLog ALL make install DESTDIR=${CT_SYSROOT_DIR}
+    CT_DoExecLog ALL make DESTDIR="${multi_root}" install
+
+    CT_Popd
 }
 
 fi
