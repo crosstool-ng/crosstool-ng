06b7d63290b3d6d27a94c2f0e8e6b7782d294a66
Alexey Brodkin <abrodkin@synopsys.com>
2019-12-26 11:53:54 +0300

    arc: Add support for ARCv3 targets
    Neither of ARCv3 processors support big-endian configurations, so
    we make that property depend on the ARC ISA version, not a bittness
    which itself has nothing to do with the endianessness.
    
    On uClibc and pages sizes. uClibc assumes default MMU size for
    ARC processors of 8Kb which was a reasonable choice for ARCompact
    (AKA ARC ISAv1) & ARCv2 ISA processors.
    
    In case of ARCv3 ISA processors default value is 4 Kb, moreover
    32-bit flavor of ARCv3 processors ARC HS58 may only have 8 Kb
    pages.
    
    And so to make sure proper page size is used, we need to propagate
    that non-default setting to uClibc. Which we do here.
    
    Note, there's no check for 64-bit flavor of ARCv3 processors
    in that change, that's because currently there're no plans for
    adding support for them into uClibc, instead glibc must be used.
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>
    Signed-off-by: Artem Panfilov <artemp@synopsys.com>

--- scripts/build/arch/arc.sh
+++ scripts/build/arch/arc.sh
@@ -2,8 +2,23 @@
 
 CT_DoArchTupleValues()
 {
-    # The architecture part of the tuple:
-    CT_TARGET_ARCH="${CT_ARCH}${CT_ARCH_SUFFIX:-${target_endian_eb}}"
+    if [ "${CT_ARCH_ARC_ISA_V3}" = "y" ]; then
+        # ARCv3 ISA is little-endian only
+        # and thus "-mlittle-endian" option in GCC is obsolete for arc64
+        CT_ARCH_ENDIAN_CFLAG=""
+        # The same goes to the linker - we only build for little-endian.
+        CT_ARCH_ENDIAN_LDFLAG=""
+        case "${CT_ARCH_BITNESS}" in
+            32)
+                CT_TARGET_ARCH="arc32${CT_ARCH_SUFFIX}"
+                ;;
+            64)
+                CT_TARGET_ARCH="arc64${CT_ARCH_SUFFIX}"
+                ;;
+        esac
+    else
+        CT_TARGET_ARCH="${CT_ARCH}${CT_ARCH_SUFFIX:-${target_endian_eb}}"
+    fi
 }
 
 CT_DoArchUClibcConfig()
@@ -11,6 +26,17 @@ CT_DoArchUClibcConfig()
     local cfg="${1}"
 
     CT_DoArchUClibcSelectArch "${cfg}" "arc"
+
+    if [ "${CT_ARCH_ARC_ISA_V3}" = "y" ]; then
+        CT_KconfigDisableOption "ARCH_BIG_ENDIAN" "${cfg}"
+        CT_KconfigDisableOption "ARCH_WANTS_BIG_ENDIAN" "${cfg}"
+        CT_KconfigEnableOption "ARCH_LITTLE_ENDIAN" "${cfg}"
+        CT_KconfigEnableOption "ARCH_WANTS_LITTLE_ENDIAN" "${cfg}"
+        # ARCv3 32-bit processors may only have page size of 4Kib
+        CT_KconfigEnableOption "CONFIG_ARC_PAGE_SIZE_4K" "${cfg}"
+        CT_KconfigDisableOption "CONFIG_ARC_PAGE_SIZE_8K" "${cfg}"
+        CT_KconfigDisableOption "CONFIG_ARC_PAGE_SIZE_16K" "${cfg}"
+    fi
 }
 
 # Multilib: Adjust configure arguments for GLIBC
