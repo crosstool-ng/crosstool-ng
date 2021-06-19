crosstool-ng/crosstool-ng

commit 1e21a30287d8d50d8425ce4debf1cf493371a6cf

gcc: Add CT_CC_GCC_TM_CLONE_REGISTRY config
This commit adds a new gcc config `CT_CC_GCC_TM_CLONE_REGISTRY` that
enables the GCC transactional memory clone registry feature for libgcc.

Note that the gcc option to control this feature is only available in
gcc 10 and above.
(see gcc commit 5a4602805eb3ebddbc935b102481e63bffc7c5e6)

Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

config/cc/gcc.in
@@ -279,6 +279,13 @@ config CC_CXA_ATEXIT
       If you get the missing symbol "__cxa_atexit" when building C++ programs,
       you might want to try disabling this option.
 
+config CC_GCC_TM_CLONE_REGISTRY
+    bool
+    prompt "Use TM clone registry"
+    depends on GCC_10_or_later
+    help
+      Enable GCC transactional memory clone registry in libgcc.
+
 config CC_GCC_DISABLE_PCH
     bool
     prompt "Do not build PCH"

scripts/build/cc/gcc.sh
@@ -396,6 +396,12 @@ do_gcc_core_backend() {
         extra_config+=("--disable-__cxa_atexit")
     fi
 
+    if [ "${CT_CC_GCC_TM_CLONE_REGISTRY}" = "y" ]; then
+        extra_config+=("--enable-tm-clone-registry")
+    else
+        extra_config+=("--disable-tm-clone-registry")
+    fi
+
     if [ -n "${CT_CC_GCC_ENABLE_CXX_FLAGS}" \
             -a "${mode}" = "baremetal" ]; then
         extra_config+=("--enable-cxx-flags=${CT_CC_GCC_ENABLE_CXX_FLAGS}")
@@ -996,6 +1002,12 @@ do_gcc_backend() {
         extra_config+=("--enable-__cxa_atexit")
     else
         extra_config+=("--disable-__cxa_atexit")
+    fi
+
+    if [ "${CT_CC_GCC_TM_CLONE_REGISTRY}" = "y" ]; then
+        extra_config+=("--enable-tm-clone-registry")
+    else
+        extra_config+=("--disable-tm-clone-registry")
     fi
 
     if [ -n "${CT_CC_GCC_ENABLE_CXX_FLAGS}" ]; then

commit 96d8a0588f8fedf881d5acd3c3ad0c3f4610c34b

gcc: Make CC_GCC_TM_CLONE_REGISTRY tristate
Explicitly passing --disable-tm-clone-registry causes gcc to create a
crtbegin.o with a zero-sized .init_array/.fini_array. This in turn
causes ld to complain.

Make CC_GCC_TM_CLONE_REGISTRY a tristate so if it's not explicitly
enabled we can let ./configure decide.

Fixes #1531

Fixes: 1e21a302 ("gcc: Add CT_CC_GCC_TM_CLONE_REGISTRY config")
Signed-off-by: Chris Packham <judge.packham@gmail.com>

config/cc/gcc.in
@@ -280,11 +280,18 @@ config CC_CXA_ATEXIT
       you might want to try disabling this option.
 
 config CC_GCC_TM_CLONE_REGISTRY
-    bool
+    tristate
     prompt "Use TM clone registry"
     depends on GCC_10_or_later
+    default m
     help
       Enable GCC transactional memory clone registry in libgcc.
+
+       Option  | tm-clone-registry  | Associated ./configure switch
+      ---------+--------------------+--------------------------------
+         Y     | forcibly used      | --enable-tm-clone-registry
+         M     | auto               | (none, ./configure decides)
+         N     | forcibly not used  | --disable-tm-clone-registry
 
 config CC_GCC_DISABLE_PCH
     bool

scripts/build/cc/gcc.sh
@@ -397,11 +397,11 @@ do_gcc_core_backend() {
         extra_config+=("--disable-__cxa_atexit")
     fi
 
-    if [ "${CT_CC_GCC_TM_CLONE_REGISTRY}" = "y" ]; then
-        extra_config+=("--enable-tm-clone-registry")
-    else
-        extra_config+=("--disable-tm-clone-registry")
-    fi
+    case "${CT_CC_GCC_TM_CLONE_REGISTRY}" in
+        y) extra_config+=("--enable-tm-clone-registry");;
+        m) ;;
+        "") extra_config+=("--disable-tm-clone-registry");;
+    esac
 
     if [ -n "${CT_CC_GCC_ENABLE_CXX_FLAGS}" \
             -a "${mode}" = "baremetal" ]; then
@@ -1021,11 +1021,11 @@ do_gcc_backend() {
         extra_config+=("--disable-__cxa_atexit")
     fi
 
-    if [ "${CT_CC_GCC_TM_CLONE_REGISTRY}" = "y" ]; then
-        extra_config+=("--enable-tm-clone-registry")
-    else
-        extra_config+=("--disable-tm-clone-registry")
-    fi
+    case "${CT_CC_GCC_TM_CLONE_REGISTRY}" in
+        y) extra_config+=("--enable-tm-clone-registry");;
+        m) ;;
+        "") extra_config+=("--disable-tm-clone-registry");;
+    esac
 
     if [ -n "${CT_CC_GCC_ENABLE_CXX_FLAGS}" ]; then
         extra_config+=("--enable-cxx-flags=${CT_CC_GCC_ENABLE_CXX_FLAGS}")