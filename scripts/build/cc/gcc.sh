278f9e6761cae53d12d5936687837b8ccbfe8d10
Joel Holdsworth <jholdsworth@nvidia.com>
2022-02-24 16:27:15 +0000

    cc/gcc: Declare "file" and "ext" variables local
    In do_gcc_core_backend and do_gcc_backend, variables "file" and "ext"
    are used to store intermediate values. Previously, these were not
    declared local. This patch corrects this issue.
    
    Signed-off-by: Joel Holdsworth <jholdsworth@nvidia.com>
    (cherry picked from commit d8249832033d63756c902b50594084df58d73c65)

--- scripts/build/cc/gcc.sh
+++ scripts/build/cc/gcc.sh
@@ -262,6 +262,8 @@ do_gcc_core_backend() {
     local -a core_targets_install
     local -a extra_user_config
     local arg
+    local file
+    local ext
 
     for arg in "$@"; do
         eval "${arg// /\\ }"
@@ -901,6 +903,8 @@ do_gcc_backend() {
     local -a final_LDFLAGS
     local tmp
     local arg
+    local file
+    local ext
 
     for arg in "$@"; do
         eval "${arg// /\\ }"
