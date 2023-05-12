f283bb6b1a877cc7ef36304aa165c55daab7d409
Yuriy Kolerov <ykolerov@synopsys.com>
2023-05-12 09:33:55 +0400

    mpfr: Add support of building MPFR for target
    It's necessary for building native GDB 13+. It depends
    on MPFR but it hasn't presented in scripts yet for building
    for target.
    
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- scripts/build/companion_libs/110-mpfr.sh
+++ scripts/build/companion_libs/110-mpfr.sh
@@ -9,7 +9,7 @@ do_mpfr_for_host() { :; }
 do_mpfr_for_target() { :; }
 
 # Overide function depending on configuration
-if [ "${CT_MPFR}" = "y" ]; then
+if [ "${CT_MPFR_TARGET}" = "y" -o "${CT_MPFR}" = "y" ]; then
 
 # Download MPFR
 do_mpfr_get() {
@@ -82,17 +82,45 @@ do_mpfr_for_host() {
     CT_EndStep
 }
 
+if [ "${CT_MPFR_TARGET}" = "y" ]; then
+do_mpfr_for_target() {
+    local -a mpfr_opts
+
+    CT_DoStep INFO "Installing MPFR for target"
+    CT_mkdir_pushd "${CT_BUILD_DIR}/build-mpfr-target-${CT_HOST}"
+
+    mpfr_opts+=( "host=${CT_TARGET}" )
+    case "${CT_TARGET}" in
+        *-*-mingw*)
+            prefix="/mingw"
+            ;;
+        *)
+            prefix="/usr"
+            ;;
+    esac
+    mpfr_opts+=( "cflags=${CT_ALL_TARGET_CFLAGS}" )
+    mpfr_opts+=( "prefix=${prefix}" )
+    mpfr_opts+=( "destdir=${CT_SYSROOT_DIR}" )
+    do_mpfr_backend "${mpfr_opts[@]}"
+
+    CT_Popd
+    CT_EndStep
+}
+fi
+
 # Build MPFR
 #     Parameter     : description               : type      : default
 #     host          : machine to run on         : tuple     : (none)
 #     prefix        : prefix to install into    : dir       : (none)
 #     cflags        : cflags to use             : string    : (empty)
 #     ldflags       : ldflags to use            : string    : (empty)
+#     destdir       : install destination       : dir       : (none)
 do_mpfr_backend() {
     local host
     local prefix
     local cflags
     local ldflags
+    local destdir
     local arg
     local -a extra_config
 
@@ -123,12 +151,34 @@ do_mpfr_backend() {
         --host=${host}                                  \
         --prefix="${prefix}"                            \
         "${extra_config[@]}"                            \
-        --with-gmp="${prefix}"                          \
+        --with-gmp="${destdir}${prefix}"                \
         --disable-shared                                \
         --enable-static
 
+    # If "${destdir}${prefix}" != "${prefix}" then it means that native MPFR
+    # is being built. In this case libgmp.la must be moved away while
+    # building MPFR. Otherwise libmpfr.la will contain this:
+    #
+    #     dependency_libs=' -L<path-to-build-dir>/lib /usr/lib/libgmp.la'
+    #
+    # Build system then tries to link MPFR with host's libgmp.a. It happens
+    # because libgmp.a and libmpfr.a are built with --prefix=/usr while
+    # cross-compiling for target and MPFR depends on GMP. In this case
+    # libtool thinks that GMP resides in /usr/lib and uses wrong path.
+    # The only way to avoid such behavior is to replace libgmp.la
+    # temporarily to force libtool using -lgmp option instead wrong one.
+    if [ "${destdir}${prefix}" != "${prefix}" ]; then
+        if [ -f ${destdir}${prefix}/lib/libgmp.la ]; then
+            mv ${destdir}${prefix}/lib/libgmp.la ${destdir}${prefix}/lib/libgmp.la.bk
+        fi
+    fi
+
     CT_DoLog EXTRA "Building MPFR"
     CT_DoExecLog ALL make ${CT_JOBSFLAGS}
+
+    if [ -f ${destdir}${prefix}/lib/libgmp.la.bk ]; then
+        mv ${destdir}${prefix}/lib/libgmp.la.bk ${destdir}${prefix}/lib/libgmp.la
+    fi
 
     if [ "${CT_COMPLIBS_CHECK}" = "y" ]; then
         if [ "${host}" = "${CT_BUILD}" ]; then
@@ -141,7 +191,7 @@ do_mpfr_backend() {
     fi
 
     CT_DoLog EXTRA "Installing MPFR"
-    CT_DoExecLog ALL make install
+    CT_DoExecLog ALL make install DESTDIR="${destdir}"
 }
 
 fi # CT_MPFR
