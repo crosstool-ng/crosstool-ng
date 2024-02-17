fb0213057527847385af15838fea4e319eba23c1
Alexey Lapshin <alexey.lapshin@espressif.com>
2024-02-17 14:10:22 +0400

    crosstool-ng: fix libstdc++ configuration for canadian builds
    libstdc++-v3/configure script can't find crt0.o to compile executable.
    After this it attempts to verify if libc features are enabled using only
    preprocessing, leading to false-positive results.
    
    This problem appeared only for canadian builds.
    
    https://github.com/crosstool-ng/crosstool-ng/issues/2110

--- scripts/crosstool-NG.sh
+++ scripts/crosstool-NG.sh
@@ -361,11 +361,16 @@ if [ -z "${CT_RESTART}" ]; then
         # binutils 2.14 and later obey it, older binutils ignore it.
         # Lets you build a working 32->64 bit cross gcc
         CT_BINUTILS_SYSROOT_ARG="--with-sysroot=${CT_SYSROOT_DIR}"
-        CT_CC_CORE_SYSROOT_ARG=("--without-headers")
-        if [ "${CT_LIBC_GLIBC}" = "y" ]; then
-            # Use --with-headers, else final gcc will define disable_glibc while
-            # building libgcc, and you'll have no profiling
-            CT_CC_SYSROOT_ARG=("--with-headers=${CT_HEADERS_DIR}")
+        if [ "${CT_CANADIAN}" = "y" -o "${CT_CROSS_NATIVE}" = "y" ]; then
+            CT_CC_CORE_SYSROOT_ARG=("--with-sysroot=${CT_SYSROOT_DIR}" "--with-native-system-header-dir=/include")
+            CT_CC_SYSROOT_ARG=("--with-sysroot=${CT_SYSROOT_DIR}" "--with-native-system-header-dir=/include")
+        else
+            CT_CC_CORE_SYSROOT_ARG=("--without-headers")
+            if [ "${CT_LIBC_GLIBC}" = "y" ]; then
+                # Use --with-headers, else final gcc will define disable_glibc while
+                # building libgcc, and you'll have no profiling
+                CT_CC_SYSROOT_ARG=("--with-headers=${CT_HEADERS_DIR}")
+            fi
         fi
     fi
     CT_DoExecLog ALL mkdir -p "${CT_SYSROOT_DIR}"
