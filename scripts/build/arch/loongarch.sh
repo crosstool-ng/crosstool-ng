89dfcf92b0a2834c2312ce4dc92aea45c383cb12
WANG Rui <wangrui@loongson.cn>
2026-09-06 13:21:51 +0800

    loongarch: add initial loongarch32 support
    Signed-off-by: WANG Rui <wangrui@loongson.cn>

--- scripts/build/arch/loongarch.sh
+++ scripts/build/arch/loongarch.sh
@@ -2,4 +2,13 @@
 
 CT_DoArchTupleValues() {
     CT_TARGET_ARCH="loongarch${CT_ARCH_BITNESS}"
+
+    case "${CT_ARCH_ABI}" in
+        *lp*f)
+            CT_TARGET_SYS="${CT_TARGET_SYS}f32"
+            ;;
+        *lp*s)
+            CT_TARGET_SYS="${CT_TARGET_SYS}sf"
+            ;;
+    esac
 }
